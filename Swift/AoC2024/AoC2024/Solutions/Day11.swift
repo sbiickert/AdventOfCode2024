3b89835bf23289e82ed211a0b3f4bdca05193510
Simon Biickert <simon@biickert.ca>
2024-12-11 10:08:54 -0700

    Day 11 Swift dead end
    After 45 iterations, 1.9 billion stones. Need a re-think.

--- Swift/AoC2024/AoC2024/Solutions/Day11.swift
+++ Swift/AoC2024/AoC2024/Solutions/Day11.swift
@@ -16,11 +16,14 @@ class Day11: AoCSolution {
 	override func solve(_ input: AoCInput) -> AoCResult {
 		super.solve(input)
 		
-		let numbers: [Int] = input.textLines.first!.split(separator: " ").map(String.init).map({Int($0)!})
+		let numbers: [Int] = input.textLines.first!
+			.split(separator: " ")
+			.map(String.init)
+			.map({Int($0)!})
 		let stones = numbers.map(Stone.init)
 		
 		let result1 = solvePart(stones, iterations: 25)
-		let result2 = "nope" //solvePart(stones, iterations: 75)
+		let result2 = solvePart(stones, iterations: 75)
 
 		return AoCResult(part1: result1, part2: result2)
 	}
@@ -28,11 +31,12 @@ class Day11: AoCSolution {
 	private func solvePart(_ stones: [Stone], iterations :Int) -> String {
 		var input = stones
 		print(input.map({$0.description}).joined(separator: " "))
-		for i in 0..<iterations {
+		let strideLength = 5
+		for i in stride(from: 1, through: iterations, by: strideLength) {
 			print("\(i)")
 			var result = [Stone]()
 			for stone in input {
-				result.append(contentsOf: stone.blink())
+				result.append(contentsOf: stone.multiBlink(strideLength))
 			}
 			input = result
 			//print(input.map(\.description).joined(separator: " "))
@@ -45,18 +49,68 @@ class Day11: AoCSolution {
 struct Stone {
 	let number: Int
 	
+	static var cache = Dictionary<Int, Dictionary<Int, [Stone]>>()
+	
+	static func getCache(number:Int, iterations:Int) -> [Stone]? {
+		if cache[number] == nil {
+			cache[number] = Dictionary<Int, [Stone]>()
+			return nil
+		}
+		if cache[number]![iterations] == nil {
+			return nil
+		}
+		return cache[number]![iterations]
+	}
+	
+	static func setCache(number:Int, iterations:Int, stones:[Stone]) {
+		if cache[number] == nil {
+			cache[number] = Dictionary<Int, [Stone]>()
+		}
+		cache[number]![iterations] = stones
+	}
+	
+	func multiBlink(_ iterations: Int) -> [Stone] {
+		guard iterations > 0 else { return [] }
+		if let cached = Stone.getCache(number: number, iterations: iterations) {
+			return cached
+		}
+		var blinked = [self]
+		for i in 1...iterations {
+			if let cached = Stone.getCache(number: number, iterations: i) {
+				blinked = cached
+			}
+			else {
+				var result = [Stone]()
+				for stone in blinked {
+					result.append(contentsOf: stone.blink())
+				}
+				blinked = result
+				Stone.setCache(number: number, iterations: i, stones: blinked)
+			}
+		}
+		return blinked
+	}
+	
 	func blink() -> [Stone] {
+		if let cached = Stone.getCache(number: number, iterations: 1) {
+			return cached
+		}
 		let numberString = "\(number)"
+		var result: [Stone]
 		if number == 0 {
-			return [Stone(number: 1)]
+			result = [Stone(number: 1)]
 		}
 		else if numberString.count.isMultiple(of: 2) {
-			return [
+			result = [
 				Stone(number: Int(numberString.prefix(numberString.count/2))!),
 				Stone(number: Int(numberString.suffix(numberString.count/2))!)
 			]
 		}
-		return [Stone(number: number * 2024)]
+		else {
+			result = [Stone(number: number * 2024)]
+		}
+		Stone.setCache(number: number, iterations: 1, stones: result)
+		return result
 	}
 	
 	var description: String {
