b5652866fd6da669eb692b1e4e5f6921210c93ed
Simon Biickert <simon@biickert.ca>
2025-01-13 13:40:18 -0700

    Update Day03.scala

--- Scala/AoC2024/src/main/scala/Day03.scala
+++ Scala/AoC2024/src/main/scala/Day03.scala
@@ -6,18 +6,26 @@ class Day03(day: Int, name: String) extends AoCLib.Solution(day, name):
     val allInput = input.mkString // Challenge input is across multiple lines
 
     solvePartOne(allInput)
-//    solvePartTwo(allInput)
+    solvePartTwo(allInput)
 
 def solvePartOne(input: String): Unit =
-  val re = """mul\((\d+),(\d+)\)""".r
-  val allMatches = re.findAllIn(input)
-  val sum = allMatches.matchData
-    .map(m => m.group(1).toInt * m.group(2).toInt)
-    .sum
-
+  val sum = sumIn(input)
   println(s"Part One: the sum of mul statements is $sum")
 
 def solvePartTwo(input: String): Unit =
-  ???
+  var sum = 0
+  val doBlocks = input.split("""do\(\)""")
+  for doBlock <- doBlocks do
+    val untilDont = doBlock.split("""don't\(\)""").head
+    sum += sumIn(untilDont)
+
+  println(s"Part Two: the sum of mul statements is $sum")
+
+def sumIn(str: String): Int =
+  val re = """mul\((\d+),(\d+)\)""".r
+  re.findAllIn(str).matchData
+    .map(m => m.group(1).toInt * m.group(2).toInt)
+    .sum
+
 
 
