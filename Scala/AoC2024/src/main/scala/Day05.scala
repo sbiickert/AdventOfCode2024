be15580aff86f8341f743d6b26c628a5fdc1a363
Simon Biickert <simon@biickert.ca>
2025-01-14 09:17:26 -0700

    Day 5 part 2 complete in Scala

--- Scala/AoC2024/src/main/scala/Day05.scala
+++ Scala/AoC2024/src/main/scala/Day05.scala
@@ -11,7 +11,7 @@ class Day05(day: Int, name: String) extends AoCLib.Solution(day, name):
     val updates = input(1).map(line => line.split(",").map(_.toInt))
 
     solvePartOne(updates, rules)
-  //solvePartTwo(input)
+    solvePartTwo(updates, rules)
 
   def solvePartOne(updates: List[Array[Int]], rules: RuleMap): Unit =
     val correct = updates.filter(isInTheCorrectOrder(_, rules))
@@ -22,12 +22,17 @@ class Day05(day: Int, name: String) extends AoCLib.Solution(day, name):
 
     println(s"Part One: the sum of the mid values is $sum")
 
-  def solvePartTwo(input: String): Unit =
-
-    println(s"Part Two: ")
+  def solvePartTwo(updates: List[Array[Int]], rules: RuleMap): Unit =
+    val incorrect = updates.filter(!isInTheCorrectOrder(_, rules))
+    val corrected = incorrect.map(fixOrder(_, rules))
+    val sum = corrected.map(update =>
+        val mid = update.length / 2
+        update(mid))
+      .sum
+    println(s"Part Two: the sum of the mid values is $sum")
 
   def parseRules(input: Iterable[String]): RuleMap =
-    var rules = mutable.HashMap.empty[Int, mutable.HashSet[Int]]
+    val rules = mutable.HashMap.empty[Int, mutable.HashSet[Int]]
     val pairs = input.map(_.split("""\|""").map(_.toInt))
     for pair <- pairs do
       if !rules.contains(pair(1)) then
@@ -45,3 +50,11 @@ class Day05(day: Int, name: String) extends AoCLib.Solution(day, name):
           return false
     isInTheCorrectOrder(update.tail, rules)
 
+  def fixOrder(update: Array[Int], rules: RuleMap): List[Int] =
+    val sortFn: (Int, Int) => Boolean = (p1,p2) =>
+      if rules.contains(p1) && rules(p1).contains(p2) then
+        false
+      else
+        true
+
+    update.toList.sortWith(sortFn)
