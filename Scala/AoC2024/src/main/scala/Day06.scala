781571d2ba78293bbcd43dbf908b487257135905
Simon Biickert <simon@biickert.ca>
2025-01-14 12:33:30 -0700

    Day 6 part 2 complete in Scala

--- Scala/AoC2024/src/main/scala/Day06.scala
+++ Scala/AoC2024/src/main/scala/Day06.scala
@@ -1,31 +1,59 @@
-import AoCLib.Direction.*
+import AoCLib.Direction.N
 import AoCLib.RotationDirection.CW
-import AoCLib.{AoCUtil, Grid, Position}
+import AoCLib.{AoCUtil, Grid, Position, Coord}
+import scala.collection.mutable
+import scala.collection.parallel.CollectionConverters._
 
 class Day06(day: Int, name: String) extends AoCLib.Solution(day, name):
   override def solve(test: Boolean, index: Int): Unit =
     val input = AoCUtil.readInput(AoCUtil.fileName(day, test))
 
-    solvePartOne(input)
-  //solvePartTwo(input)
+    val visitedCoords = solvePartOne(input)
+    solvePartTwo(input, visitedCoords)
 
-  def solvePartOne(input: List[String]): Unit =
-    val map = Grid.load(input)
-    val startCoord = map.coords(withValue = Some("^")).head
+  def solvePartOne(input: List[String]): List[Coord] =
+    val grid = Grid.load(input)
+    val startCoord = grid.coords(withValue = Some("^")).head
     var pos = Position(coord = startCoord, direction = N)
-    var ext = map.extent.get
+    val ext = grid.extent.get
     while (ext.contains(pos.coord)) do
-      map.set(pos.coord, "X")
-      val ahead = map.getString(pos.coord.offset(pos.direction))
-      if ahead == "#" then
-        pos = pos.turn(CW)
-      else
-        pos = pos.moveForward()
+      grid.set(pos.coord, "X")
+      pos = makeMove(pos, grid)
 
-    val visitedCount = map.coords(withValue = Some("X")).size
-
+    val visited = grid.coords(withValue = Some("X"))
+    val visitedCount = visited.size
     println(s"Part One: the number of spots visited is $visitedCount")
+    visited.filterNot(_ == startCoord).toList
 
-  def solvePartTwo(input: String): Unit =
+  def solvePartTwo(input: List[String], visited: List[Coord]): Unit =
+    val grid = Grid.load(input)
+    val startCoord = grid.coords(withValue = Some("^")).head
+    val pos = Position(coord = startCoord, direction = N)
 
-    println(s"Part Two: ")
+    // Using Parallel Collection for speed
+    // https://docs.scala-lang.org/overviews/parallel-collections/overview.html
+    val loopOrNot = visited.par.map(c => doesItLoop(pos, grid, barrier = c))
+    val loopingCount = loopOrNot.count(_ == true)
+
+    println(s"Part Two: the number of spots that cause a loop is $loopingCount")
+
+  private def makeMove(pos: Position, map: Grid, barrier: Option[Coord] = None): Position =
+    val aheadCoord = pos.coord.offset(pos.direction)
+    var aheadValue = map.getString(aheadCoord)
+    if barrier.nonEmpty && barrier.get == aheadCoord then
+      aheadValue = "#"
+    if aheadValue == "#" then
+      pos.turn(CW)
+    else
+      pos.moveForward()
+
+  private def doesItLoop(pos: Position, grid: Grid, barrier: Coord): Boolean =
+    var position = pos
+    val history = mutable.HashSet.empty[Position]
+    var loops = true
+    while (loops && !history.contains(position)) do
+      history.addOne(position)
+      position = makeMove(position, grid, Some(barrier))
+      if !grid.extent.get.contains(position.coord) then
+        loops = false
+    loops
