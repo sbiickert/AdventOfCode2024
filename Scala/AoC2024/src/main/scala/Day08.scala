0a2e7196c6e3208cbcff6f8c173077d186261972
Simon Biickert <simon@biickert.ca>
2025-01-21 22:04:28 -0700

    Update Day08.scala
    Changed part 2 to use recursion

--- Scala/AoC2024/src/main/scala/Day08.scala
+++ Scala/AoC2024/src/main/scala/Day08.scala
@@ -1,6 +1,8 @@
 import AoCLib.AdjacencyRule.Queen
 import AoCLib.{AoCUtil, Coord, Extent, Grid}
 
+import scala.annotation.tailrec
+
 class Day08(day: Int, name: String) extends AoCLib.Solution(day, name):
   override def solve(test: Boolean, index: Int): Unit =
     val input = AoCUtil.readGroupedInput(AoCUtil.fileName(day, test))(0)
@@ -8,7 +10,6 @@ class Day08(day: Int, name: String) extends AoCLib.Solution(day, name):
     val coordsByValue = grid.histogram(includeUnset = false)
       .map((k:String, _:Int) => (k, grid.coords(withValue = Some(k))))
 
-    //grid.print()
     solvePartOne(coordsByValue, grid.extent.get)
     solvePartTwo(coordsByValue, grid.extent.get)
 
@@ -21,7 +22,6 @@ class Day08(day: Int, name: String) extends AoCLib.Solution(day, name):
         if extent.contains(antiNode0) then antiNodes.set(antiNode0, "#")
         if extent.contains(antiNode1) then antiNodes.set(antiNode1, "#")
 
-    //antiNodes.print()
     val antiNodeCount = antiNodes.coords(withValue = Some("#")).size
     println(s"Part One: the number of antinodes is $antiNodeCount")
 
@@ -30,21 +30,14 @@ def solvePartTwo(nodes: Map[String, Iterable[Coord]], extent: Extent): Unit =
   val antiNodes = Grid()
   for (_, coords) <- nodes do
     for combo <- coords.toList.combinations(2) do
-      antiNodes.set(combo(0), "#")
-      antiNodes.set(combo(1), "#")
-      val delta0 = combo(1).delta(combo(0))
-      val delta1 = combo(0).delta(combo(1))
-      
-      var antiNode = combo(0) + delta0
-      while extent.contains(antiNode) do
-        antiNodes.set(antiNode, "#")
-        antiNode = antiNode + delta0
+      setAntinodes(antiNodes, combo(0), combo(1).delta(combo(0)), extent)
+      setAntinodes(antiNodes, combo(1), combo(0).delta(combo(1)), extent)
 
-      antiNode = combo(1) + delta1
-      while extent.contains(antiNode) do
-        antiNodes.set(antiNode, "#")
-        antiNode = antiNode + delta1
-
-  //antiNodes.print()
   val antiNodeCount = antiNodes.coords(withValue = Some("#")).size
   println(s"Part Two: the number of antinodes is $antiNodeCount")
+
+@tailrec
+def setAntinodes(grid: Grid, coord: Coord, delta: Coord, limit: Extent):Unit =
+  if !limit.contains(coord) then return
+  grid.set(coord, "#")
+  setAntinodes(grid, coord + delta, delta, limit)
