9911a6c6d5daa1c86976313bfb4f7ec403d3cfa0
Simon Biickert <simon@biickert.ca>
2025-02-03 12:57:03 -0700

    Additional F# unit testing

--- F#/AoC2024/AoCGeometryTest.fsx
+++ F#/AoC2024/AoCGeometryTest.fsx
@@ -6,8 +6,12 @@ let assertEqual a b =
 
 let assertTrue a =
     if a <> true then failwith $"Assertion was not true."
+let assertFalse a =
+    if a <> false then failwith $"Assertion was not false."
 
 open AoC.Geometry
+
+// Test Direction here
 
 let testCoordCreate () =
     let c = mkCoord 10 30
@@ -40,5 +44,32 @@ let testCoordDistance () =
     assertTrue (AoC.Util.approxEqual 0.01 d 11.18)
 
 testCoordDistance ()
+
+let testCoordOffsets () = 
+    assertEqual {x = 0; y = -1} (Coord.offset Direction.N 1L Coord.origin)
+    assertEqual {x = -1; y = 1} (Coord.offset Direction.SW 1L Coord.origin)
+
+testCoordOffsets ()
+
+let testCoordAdjacency () =
+    let cHorz = Direction.offset Direction.E
+    assertTrue (Coord.areAdjacent cHorz Coord.origin AdjacencyRule.Rook)
+    assertFalse (Coord.areAdjacent cHorz Coord.origin AdjacencyRule.Bishop)
+    assertTrue (Coord.areAdjacent cHorz Coord.origin AdjacencyRule.Queen)
+    let cDiag = Direction.offset Direction.SE
+    assertFalse (Coord.areAdjacent cDiag Coord.origin AdjacencyRule.Rook)
+    assertTrue (Coord.areAdjacent cDiag Coord.origin AdjacencyRule.Bishop)
+    assertTrue (Coord.areAdjacent cDiag Coord.origin AdjacencyRule.Queen)
+    let cFar = {x = 2; y = 0}
+    assertFalse (Coord.areAdjacent cFar Coord.origin AdjacencyRule.Queen)
+
+    let rookAdj = Coord.adjacentCoords Coord.origin AdjacencyRule.Rook
+    assertEqual 4 rookAdj.Length
+    let bishopAdj = Coord.adjacentCoords Coord.origin AdjacencyRule.Bishop
+    assertEqual 4 bishopAdj.Length
+    let queenAdj = Coord.adjacentCoords Coord.origin AdjacencyRule.Queen
+    assertEqual 8 queenAdj.Length
+
+testCoordAdjacency ()
 
 printfn $"**** All tests passed. ****"
