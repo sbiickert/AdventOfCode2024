b2ab43f79428771e0984f7bdf5d1572291ec50a7
Simon Biickert <simon@biickert.ca>
2025-02-05 11:41:03 -0700

    Day 2 complete in F#

--- F#/AoC2024/Day02.fs
+++ F#/AoC2024/Day02.fs
@@ -10,30 +10,65 @@ let parseInput (input:string list) =
         |> Array.map (fun s -> int s) 
         |> Array.toList)
 
+let positivePredicate a = a > 0 && a <= 3
+let negativePredicate a = a < 0 && a >= -3
+
 let allPositive trend: bool =
-    let filtered =
-        trend |> List.filter (fun a -> a > 0 && a <= 3)
-    filtered.Length = trend.Length
+    trend |> List.forall positivePredicate
 
 let allNegative trend: bool =
-    let filtered =
-        trend |> List.filter (fun a -> a < 0 && a >= -3)
-    filtered.Length = trend.Length
+    trend |> List.forall negativePredicate
+
+let isSafe trend: bool =
+    (allPositive trend) || (allNegative trend)
+
+let findUnsafeValueIndex (trend: int list) =
+    if trend.Head > 0 then
+        List.findIndex (positivePredicate >> not) trend
+    else
+        List.findIndex (negativePredicate >> not) trend
+
+let getTrend report = 
+    report
+    |> List.windowed 2
+    |> List.map (fun [a;b] -> b - a )
+
+let getTrends reports =
+    reports
+    |> List.map getTrend
+
+let mkReport trend =
+    trend |> List.scan (fun acc t -> acc + t) 0 
 
 let solvePartOne (reports: int list list) =
-    let trends =
-        reports
-        |> List.map (fun rpt -> 
-            List.windowed 2 rpt 
-            |> List.map (fun [a;b] -> b - a ))
-    let ascending = 
-        trends |> List.filter allPositive
-    let descending =
-        trends |> List.filter allNegative
-    ascending.Length + descending.Length
+    let trends = getTrends reports
+    let safeTrends = trends |> List.filter isSafe
+    safeTrends.Length
 
-let solvePartTwo input =
-    2
+let solvePartTwo (reports: int list list) pt1SafeCount =
+    let trends = getTrends reports
+    let unsafeTrends = trends |> List.filter (isSafe >> not)
+
+    let mutable countUnsafeMadeSafe = 0
+    for trend in unsafeTrends do
+        let mutable canBeMadeSafe = false
+        let badIndex = findUnsafeValueIndex trend
+        //printfn $"The bad index in {trend} is {badIndex}"
+        let report = mkReport trend
+        let imin = max 0 (badIndex-1)
+        let imax = badIndex+1
+        for i in imin .. imax do
+            let before = List.take i report
+            let after = List.skip (i+1) report
+            let modifiedReport = before @ after
+            let modifiedTrend = getTrend modifiedReport
+            if isSafe modifiedTrend then
+                //printfn $"Removing number at {i} made {modifiedReport} safe."
+                canBeMadeSafe <- true
+        if canBeMadeSafe then
+            countUnsafeMadeSafe <- countUnsafeMadeSafe + 1
+
+    pt1SafeCount + countUnsafeMadeSafe
 
 let solveDay02 isTest: Unit =
     let day = 2
@@ -47,6 +82,7 @@ let solveDay02 isTest: Unit =
     let solution1 = solvePartOne reports
     printfn $"The number of safe reports is {solution1}"
 
-    let solution2 = solvePartTwo input
+    let solution2 = solvePartTwo reports solution1
+    printfn $"The number of safe reports with dampening is {solution2}"
 
     printfn "All done."
