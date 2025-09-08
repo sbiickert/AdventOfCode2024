1250196184ba90ef738b8f901b460d4726b31903
Simon Biickert <simon@biickert.ca>
2025-09-08 17:03:28 -0600

    Day 2 complete in Pascal

--- Pascal/day02.pas
+++ Pascal/day02.pas
@@ -8,31 +8,65 @@ Uses SysUtils, StrUtils, AoCUtils, Classes;
 
 Type
 	IArr2D = array of array of Integer;
+	IArr1D = array of Integer;
 
 Const
 	DAY = 2;
 
+Function ReportToTrend(report: IArr1D; ignoringIndex: Integer = -1): IArr1D;
+Var
+	i,j,k,diff: Integer;
+Begin
+	if ignoringIndex >= 0 then 
+		SetLength(result, Length(report)-2)
+	else
+		SetLength(result, Length(report)-1);
+		
+	i := 0; j := 0; k := 0;
+	while j <= High(report) do
+	begin
+		if i = ignoringIndex then Inc(i);
+		j := i + 1;
+		if j = ignoringIndex then Inc(j);
+		diff := report[j] - report[i];
+// 		Write( Format('%d ', [diff]) );
+		result[k] := diff;
+		Inc(i); Inc(j); Inc(k);
+	end;
+// 	WriteLn;
+End;
+
 Function ReportsToTrends(reports: IArr2D): IArr2D;
 Var
-	i,j,diff: Integer;
+	i: Integer;
 Begin
-	SetLength(result, Length(reports), 7);
+	SetLength(result, Length(reports));
 	for i := 0 to Length(reports)-1 do
 	begin
-		SetLength(result[i], Length(reports[i])-1);
-		for j := 0 to High(reports[i])-1 do
-		begin
-			diff := reports[i,j+1] - reports[i,j];
-			// Write( Format('%d ', [diff]) );
-			result[i,j] := diff;
-		end;
-		// WriteLn;
+		result[i] := ReportToTrend(reports[i]);
 	end;
+End;
+
+Function IsTrendSafe(trend: IArr1D): Boolean;
+Var
+	j,posCount,negCount: Integer;
+Begin
+	result := True;
+	posCount := 0;
+	negCount := 0;
+	for j := 0 to High(trend) do
+	begin
+		if Abs(trend[j]) > 3 then result := False
+		else if trend[j] > 0 then Inc(posCount)
+		else if trend[j] < 0 then Inc(negCount)
+		else result := False; // zero
+	end;
+	if (posCount > 0) and (negCount > 0) then result := False;
 End;
 
 Procedure SolvePart1(reports: IArr2D);
 Var
-	i,j,safeCount,posCount,negCount: Integer;
+	i,safeCount: Integer;
 	trends: IArr2D;
 	isSafe: Boolean;
 Begin
@@ -42,29 +76,42 @@ Begin
 	trends := ReportsToTrends(reports);
 	for i := 0 to High(trends) do
 	begin
-		isSafe := True;
-		posCount := 0;
-		negCount := 0;
-		for j := 0 to High(trends[i]) do
-		begin
-			if Abs(trends[i,j]) > 3 then isSafe := False
-			else if trends[i,j] > 0 then Inc(posCount)
-			else if trends[i,j] < 0 then Inc(negCount)
-			else isSafe := False; // zero
-		end;
-		if (posCount > 0) and (negCount > 0) then isSafe := False;
+		isSafe := IsTrendSafe(trends[i]);
 		if isSafe then Inc(safeCount);
 	end;
 	
 	WriteLn(Format('Part One Solution: %d', [safeCount]));
 End;
 
-Procedure SolvePart2(values: IArr2D);
+Procedure SolvePart2(reports: IArr2D);
 Var
-	a, b, c: Integer;
+	i,j,safeCount: Integer;
+	trend: IArr1D;
+	isSafe: Boolean;
 Begin
-	WriteLn('Part 2: DESCRIPTION');
-	WriteLn(Format('Part Two Solution: %d', [13]));
+	WriteLn('Part 2: How many reports are safe with the Problem Dampener?');
+	
+	safeCount := 0;
+	
+	for i := 0 to High(reports) do
+	begin
+		trend := ReportToTrend(reports[i]);
+		if IsTrendSafe(trend) then
+			Inc(safeCount)
+		else
+			for j := 0 to High(reports[i]) do
+			begin
+				trend := ReportToTrend(reports[i], j);
+				if IsTrendSafe(trend) then
+				begin
+					Inc(safeCount);
+// 					WriteLn( Format('Report %d is made safe by removing value %d', [i,j]));
+					break;
+				end;
+			end;
+	end;
+	
+	WriteLn(Format('Part Two Solution: %d', [safeCount]));
 End;
 
 Function ParseReports(input: TStringList): IArr2D;
@@ -92,11 +139,6 @@ Begin
 	iFileName := InputFileName(DAY, False);
 	input := ReadInput(iFileName);
 	reports := ParseReports(input);
-// 	AssertIntEqual(reports[0,0], 7, '');
-// 	AssertIntEqual(reports[0,4], 1, '');
-// 	AssertIntEqual(reports[5,0], 1, '');
-// 	AssertIntEqual(reports[5,4], 9, '');
-// 	AssertIntEqual(High(reports[5]), 4, '');
 	SolvePart1(reports);
 	SolvePart2(reports);
 End.
