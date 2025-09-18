839814546a84720f7b397c345a8049d26646d00f
Simon Biickert <simon@biickert.ca>
2025-09-18 10:24:55 -0600

    Day 9 complete in Pascal

--- Pascal/day09.pas
+++ Pascal/day09.pas
@@ -9,14 +9,23 @@ Uses SysUtils, StrUtils, AoCUtils, Classes;
 Const
 	DAY = 9;
 
-Function CheckSumForDiskMap(diskMap: AoCStringArray): Integer;
+Type
+	FileInfo = Record
+		id: String;
+		start: Integer;
+		size: Integer;
+	end;
+	FileInfoArray = array of FileInfo;
+
+Function CheckSumForDiskMap(diskMap: AoCStringArray): Int64;
 Var
 	blockPtr: Integer;
 Begin
 	result := 0;
 	for blockPtr := 0 to High(diskMap) do
 	begin
-		if diskMap[blockPtr] = '.' then break;
+		if diskMap[blockPtr] = '.' then continue;
+// 		WriteLn(Format('%d + (%d * %s)', [result, blockPtr, diskMap[blockPtr]]));
 		result := result + (blockPtr * StrToInt(diskMap[blockPtr]));
 	end;
 End;
@@ -24,7 +33,7 @@ End;
 Procedure SolvePart1(diskMap: AoCStringArray);
 Var
 	spacePtr,blockPtr: Integer;
-	checkSum: Integer;
+	checkSum: Int64;
 Begin
 	WriteLn('Part 1: What is the filesystem checksum after compacting blocks?');
 	
@@ -45,12 +54,98 @@ Begin
 	WriteLn(Format('Part One Solution: %d', [checkSum]));
 End;
 
-Procedure SolvePart2(values: TStringList);
+
+Function GetFileInfo(diskMap: AoCStringArray): FileInfoArray;
 Var
-	a, b, c: Integer;
+	ptrStart,ptr: Integer;
+	id,valueAtPtr: String;
+	info: FileInfo;
 Begin
-	WriteLn('Part 2: DESCRIPTION');
-	WriteLn(Format('Part Two Solution: %d', [13]));
+	result := [];
+	id := '.';
+	ptrStart := -1;
+	PushString(diskMap, '.'); // Add a space at the end 
+	
+	for ptr := 0 to High(diskMap) do
+	begin
+		valueAtPtr := diskMap[ptr];
+		if valueAtPtr <> id then
+		begin
+			if id <> '.' then
+			begin
+				info.id := id;
+				info.start := ptrStart;
+				info.size := ptr - ptrStart;
+				SetLength(result, Length(result)+1);
+				result[High(result)] := info;
+			end;
+			id := valueAtPtr;
+			ptrStart := ptr;
+		end;
+	end;
+End;
+
+Function FindFirstSpace(diskMap: AoCStringArray; requestedSize: Integer): Integer;
+Var
+	ptrStart,ptr: Integer;
+	id,valueAtPtr: String;
+	spaceSize: Integer;
+Begin
+	id := '';
+	result := -1;
+	ptrStart := -1;
+	
+	for ptr := 0 to High(diskMap) do
+	begin
+		valueAtPtr := diskMap[ptr];
+		if valueAtPtr <> id then
+		begin
+			if id = '.' then
+			begin
+				spaceSize := ptr - ptrStart;
+				if spaceSize >= requestedSize then
+				begin
+					result := ptrStart;
+					exit;
+				end;
+			end;
+			id := valueAtPtr;
+			ptrStart := ptr;
+		end;
+	end;
+End;
+
+
+Procedure SolvePart2(diskMap: AoCStringArray);
+Var
+	fiArr: FileInfoArray;
+	fi: FileInfo;
+	i,j,spaceStart,s,f: Integer;
+	checkSum: Int64;
+Begin
+	WriteLn('Part 2: What is the filesystem checksum after compacting files?');
+	
+	fiArr := GetFileInfo(diskMap);
+	for i := High(fiArr) downto Low(fiArr) do
+	begin
+		fi := fiArr[i];
+		spaceStart := FindFirstSpace(diskMap, fi.size);
+		if (spaceStart >= 0) and (spaceStart < fi.start) then
+		begin
+			s := spaceStart; f := fi.start;
+			for j := 1 to fi.size do
+			begin
+				diskMap[s] := diskMap[f];
+				diskMap[f] := '.';
+				Inc(s); Inc(f);
+			end;
+		end;
+// 		WriteLn(JoinStrArray('', diskMap));
+	end;
+	
+	checkSum := CheckSumForDiskMap(diskMap);
+	
+	WriteLn(Format('Part Two Solution: %d', [checkSum]));
 End;
 
 Function StrToDiskMap(str: String): AoCStringArray;
@@ -83,9 +178,10 @@ Var
 	diskMap: AoCStringArray;
 Begin
 	WriteLn('AoC 2015 Day 09: Disk Fragmenter');
-	iFileName := InputFileName(DAY, True);
-	input := ReadGroupedInput(iFileName, 1);
+	iFileName := InputFileName(DAY, False);
+	input := ReadGroupedInput(iFileName, 0);
 	diskMap := StrToDiskMap(input[0]);
 	SolvePart1(diskMap);
-	SolvePart2(input);
+	diskMap := StrToDiskMap(input[0]);
+	SolvePart2(diskMap);
 End.
